be566cfe77e827455f2c39e7c826940cdc628932
Matthew Thornton <matthewthornton.usa@gmail.com>
2016-03-08 11:45:42 -0800

    setting up dependencies

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'jade');
+
 app.get('/', function(request, response) {
-    response.write('Hello world');
-    response.end();
+    console.log("someone made a request.");
+    response.sendFile(__dirname + '/public/index');
 });
 
 app.listen(3000, function() {
     // curl http://localhost:3000/
+    // curl -i http://localhost:3000/blocks
     console.log('listening on port 3000');
 });
-
