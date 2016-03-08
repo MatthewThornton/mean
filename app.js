1182c2000b41db67952a96900728491ad7241da2
Matthew Thornton <matthewthornton.usa@gmail.com>
2016-03-08 14:57:49 -0800

    params post checks for routing

--- app.js
+++ app.js
@@ -13,19 +13,47 @@ var blocks = {
     'Rotating': 'Moving in a circle around its center'
 };
 
+var locations = {
+    'Fixed': 'First floor',
+    'Movable': 'Second floor',
+    'Rotating': 'Penthouse'
+};
+
+app.param('name', function(request, response, next){
+    var name = request.params.name;
+    var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
+
+    request.blockName = block;
+
+    next();
+});
+
+app.get('/blocks', function(request, response){
+    if (request.query.limit >= 0) {
+        response.json(blocks.slice(0, request.query.limit));
+    }else {
+        response.json(Object.keys(blocks));
+    }
+});
+
 app.get('/blocks/:name', function(request, response) {
-    var description = blocks[request.params.name];
+    var description = blocks[request.blockName];
     if (!description) {
         response.status(404).json('No description found for ' + request.params.name);
     }else{
         response.json(description);
     }
-    if (request.query.limit >= 0) {
-        response.json(blocks.slice(0, request.query.limit));
+});
+
+app.get('/locations/:name', function(request, response){
+    var location = locations[request.blockName];
+    if (!location) {
+        response.status(404).json('No location found for ' + request.params.name);
     }else {
-        response.json(blocks);
+        response.json(location);
     }
 });
+
 
 app.listen(3000, function() {
     // curl http://localhost:3000/
