3c8f4f3d59d96b30d612f6db4c01599d6d8e7915
Matthew Thornton <matthewthornton.usa@gmail.com>
2016-03-09 09:34:44 -0800

    update routes using an export module

--- app.js
+++ app.js
@@ -1,48 +1,16 @@
 var express = require('express');
 var app = express();
-
-var bodyParser = require('body-parser');
-var parseUrlencoded = bodyParser.urlencoded({ extended: false });
-
-app.route('/blocks')
-    .get(function(request, response){
-        if (request.query.limit >= 0) {
-            response.json(blocks.slice(0, request.query.limit));
-        }else {
-            response.json(Object.keys(blocks));
-        }
-    })
-    .post(parseUrlencoded, function(request, response){
-        var newBlock = request.body;
-        blocks[newBlock.name] = newBlock.description; // <input name='name'><input name='description'>
-
-        response.status(201).json(newBlock.name);
-    });
-app.route('/blocks/:name')
-    .get(function(request, response){
-        var description = blocks[request.blockName];
-        if (!description) {
-            response.status(404).json('No description found for ' + request.params.name);
-        }else{
-            response.json(description);
-        }
-    })
-    .delete(function(request, response){
-        delete blocks[request.blockName];
-        response.sendStatus(200); //sets response body to 'OK'
-    });
-
-var logger = require('./logger');
-app.use(logger);
 app.use(express.static('public'));
-
 app.set('view engine', 'jade'); //TODO this doesn't work?
 
-var blocks = {
-    'Fixed': 'Fastened securely in position',
-    'Movable': 'Capable of being moved',
-    'Rotating': 'Moving in a circle around its center'
-};
+// Routes
+var blocks = require('./routes/blocks');
+app.use('/blocks', blocks);
+
+// Logger
+var logger = require('./logger');
+app.use(logger);
+
 
 var locations = {
     'Fixed': 'First floor',
@@ -50,15 +18,6 @@ var locations = {
     'Rotating': 'Penthouse'
 };
 
-// Post checks
-app.param('name', function(request, response, next){
-    var name = request.params.name;
-    var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
-
-    request.blockName = block;
-
-    next();
-});
 
 app.get('/locations/:name', function(request, response){
     var location = locations[request.blockName];
